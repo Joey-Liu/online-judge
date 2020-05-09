aa94bac7bc705471c9b44c27e18407b62eed0aff
joey <joey_liucoder@163.com>
2020-05-09 23:49:25 +0800

    windows 同步代码

---
+++ codeforces/round 635 div2/A/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <queue>
+#include <set>
+#include <math.h>
+#include <functional>
+#include <limits.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int> PII;
+
+const int maxn = 100000 + 10;
+int nums[maxn];
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int kasenum;
+	cin >> kasenum;
+	while (kasenum--) {
+		LL a, b, c, d;
+		cin >> a >> b >> c >> d;
+		cout << b << " " << c << " " << c << endl;
+	}
+	return 0;
+}
